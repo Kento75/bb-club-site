6d71550fa9853a043942c8fda5a0936616ee21f8
Kento75 <kento2github@gmail.com>
2018-11-06 06:52:25 +0900

    Add seeds datas and mock create command

--- db/seeds/development/members.rb
+++ db/seeds/development/members.rb
@@ -8,22 +8,7 @@ gnames = ["太郎", "次郎", "花子"]
     full_name: "#{fnames[idx % 4]} #{gnames[idx % 3]}",
     email: "#{names[idx]}@example.com",
     birthday: "1981-12-01",
-    gender: [0, 0, 1][idx % 3],
-    administrator: (idx == 0),
-    password: "asagao!",
-    password_confirmation: "asagao!"
-  )
-end
-0.upto(29) do |idx|
-  Member.create(
-    number: idx + 20,
-    name: "John#{idx + 1}",
-    full_name: "John Doe#{idx + 1}",
-    email: "John#{idx+1}@example.com",
-    birthday: "1981-12-01",
-    gender: 0,
-    administrator: false,
-    password: "password",
-    password_confirmation: "password"
+    sex: [1, 1, 2][idx % 3],
+    administrator: (idx == 0)
   )
 end
