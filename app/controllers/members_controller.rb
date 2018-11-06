26d977fcc0a9bb54234b6ceff5263f4a8f77a99e
Kento75 <kento2github@gmail.com>
2018-11-06 20:30:27 +0900

    Add memberlist component

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,22 +1,25 @@
 class MembersController < ApplicationController
-  before_action :login_required
 
-  # 会員一覧
+  # 会員一覧画面
   def index
     @members = Member.order("number")
-      .page(params[:page]).per(15)
   end
-
-  # 検索
-  def search
-    @members = Member.search(params[:q])
-      .page(params[:page]).per(15)
-
-    render "index"
-  end
-
-  # 会員情報の詳細
+  
   def show
-    @member = Member.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
