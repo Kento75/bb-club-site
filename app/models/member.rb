d09ff72fa3c8a4cb2027e1bd61c7278958bfc35e
Kento75 <kento2github@gmail.com>
2018-11-08 06:08:28 +0900

    Add validator

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,28 @@
 class Member < ApplicationRecord
+
+  # 背番号バリデータ
+  validates :number, presence: true,
+    numericality: {
+      only_integer: true,
+      greater_than: 0,
+      less_than: 100,
+      allow_blank: true
+    },
+    uniqueness: true
+  
+  # ユーザー名バリデータ
+  validates :name, presence: true,
+    format: { with: /\A[A-Za-z][A-Za-z0-9]*\z/, allow_blank: true },
+    length: { minimum: 2, maximum: 20, allow_blank: true },
+    uniqueness: { case_sensitive: false }
+
+  # 氏名バリデータ
+  validates :full_name, presence: true,
+    length: { maximum: 20 }
+  
+  # メールアドレスバリデータ
+  validates :email, email: { allow_blank: true }
+
   class << self
     def search(query)
       rel = order("number")
